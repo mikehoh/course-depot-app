edfb873266bfe71ce7be5659f01e09ad34b5396e
test <test>
2012-12-04 19:42:03 +0200

    Initial commit rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseDepotApp::Application.config.secret_token = 'a7b4ce3854f349c75d8dadedb1d9d2146f23f79412d5c46b6cac6814f300109caee8117acd26fc7d1f56732dc02ae9cd7a4abdfd9829d18ca5acd388817f7a3c'
