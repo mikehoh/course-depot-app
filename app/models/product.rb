9721d8fc9ea071a0878ca5548a0e64026956c5aa
pavel morozov <mikehoh@gmail.com>
2012-12-06 20:15:40 +0200

    add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :imageurl, :price, :presence=>true
+	validates :title, :uniqueness=>true
+	validates :price, :numericality=>{:greater_than_or_equal_to=>0.01}
+	validates :imageurl, :format=>{:with=>%r{\.(gif|jpg|png)$}i,:message=>"Must be JPG, PNG or GIF"}
 end
