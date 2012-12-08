0432efb7c83030b69c82638e97545584bfa03173
pavel morozov <mikehoh@gmail.com>
2012-12-08 11:56:47 +0200

    add line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,23 @@
 class Product < ActiveRecord::Base
-	default_scope :order=>'price'
+
+	has_many :line_items
+	
+#	default_scope :order=>'price'
 #	scope :cheap, lambda{where(:price<10)}
-	scope :cheap, where('price<100')
+#	scope :cheap, where('price<100')
 
 	validates :title, :description, :imageurl, :price, :presence=>true
 	validates :title, :uniqueness=>true
 	validates :price, :numericality=>{:greater_than_or_equal_to=>0.01}
 	validates :imageurl, :format=>{:with=>%r{\.(gif|jpg|png)$}i,:message=>"Must be JPG, PNG or GIF"}
+	
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			true
+		else
+			errors.add(:base,'There are line items')
+			false
+		end
+	end
+	
 end
